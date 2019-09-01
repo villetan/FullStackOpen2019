6e8c41d7de13c14cc87c2f122f624616792e8e03
villetan <ville.tanskanen@helsinki.fi>
2019-09-01 15:35:30 +0300

    commit before exercises

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,39 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
   return (
     <div>
-      <p>
-        Hello {props.name},
-         you are {props.age} years old
-      </p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
-const Footer = () => {
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   return (
     <div>
-      greeting app created by 
-      <a href="https://github.com/mluukkai">mluukkai</a>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left' />
+        <Button onClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
     </div>
   )
 }
 
-const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="MAYA" age={10 + 20}/>
-      <Hello name={nimi} age={ika} />
-      <Footer />
-    </>
-  )
-}
-
-ReactDOM.render(<App />,
- document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
